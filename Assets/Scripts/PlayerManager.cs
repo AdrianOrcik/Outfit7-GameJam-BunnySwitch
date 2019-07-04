45e4cfb55e005f6f7cc206fc902f821f68d21bea
TadejaK <tadeja.kersic@gmail.com>
2019-07-04 18:10:02 +0100

    Player movement

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public Player player;
+    public float paylerSpeed = 5;
+
+    void Start()
+    {
+        player = Instantiate(player);
+    }
+    void Update()
+    {
+        player.transform.Translate (Vector3.right * paylerSpeed * Time.deltaTime); 
+    }
+
+    public void Jump()
+    {
+        Debug.Log("jump");
+    }
+}
